c24ef538f65e1b03cd9f363619cb044c507d1f76
hananitallyson <tallysonhanani@gmail.com>
2026-08-12 12:34:06 -0300

    fix: wallpaper autostart

--- hypr/modules/wallpaper.lua
+++
@@ -1,4 +0,0 @@
-local wallpaper = os.getenv("HOME") .. "/.config/hypr/wallpapers/TSMC-WALLPAPER.jpg"
-
-hl.exec_cmd("hyprpaper")
-hl.exec_cmd("hyprctl hyprpaper wallpaper '," .. wallpaper .. ",cover'")
