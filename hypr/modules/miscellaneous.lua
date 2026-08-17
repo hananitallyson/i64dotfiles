7bef17963f6d76e94648b23ec6b16f2688ce6398
hananitallyson <tallysonhanani@gmail.com>
2026-08-17 13:18:27 -0300

    feat: wallpaper

--- hypr/modules/miscellaneous.lua
+++ hypr/modules/miscellaneous.lua
@@ -1,5 +1,6 @@
 hl.config({
     misc = {
+        background_color = 0x000000,
         force_default_wallpaper = 0,
         disable_hyprland_logo = true,
         disable_splash_rendering = true,
