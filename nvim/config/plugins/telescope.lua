fce8acb1f467c943f640ef85a8cf7a073e1ab902
hananitallyson <tallysonhanani@gmail.com>
2026-09-11 17:06:56 -0300

    fix: telescope colors

--- nvim/config/plugins/telescope.lua
+++ nvim/config/plugins/telescope.lua
@@ -1,15 +1,23 @@
 vim.pack.add({
-	"https://github.com/nvim-lua/plenary.nvim",
-	"https://github.com/nvim-telescope/telescope.nvim",
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/nvim-telescope/telescope.nvim",
 })
 
 require("telescope").setup({
-	defaults = {
-		layout_strategy = "horizontal",
-		sorting_strategy = "ascending",
-		layout_config = {
-			prompt_position = "top",
-		},
-		borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-	},
+  defaults = {
+    layout_strategy = "horizontal",
+
+    sorting_strategy = "ascending",
+
+    layout_config = {
+      width = 0.95,
+      height = 0.85,
+      preview_width = 0.60,
+      preview_cutoff = 0,
+      prompt_position = "top",
+      mirror = false,
+    },
+
+    borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+  },
 })
